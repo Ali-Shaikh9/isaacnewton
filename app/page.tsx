2663541008195c1fec87cd69841abb3e6e17acd1
Ali-Shaikh9 <shaikhalimurtuza13@gmail.com>
2026-08-15 20:59:12 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,294 @@
+import { ArrowDown, ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main>
+      <section
+        id="#"
+        className="mt-12 py-24 w-full bg-linear-to-b from-black to-neutral-800"
+      >
+        <div className="flex md:flex-col md:flex-row items-center justify-between max-w-7xl mx-auto p-8">
+          <div className="">
+            <h1 className="text-6xl font-bold uppercase">
+              The man who <br /> changed how we <br /> understand the <br />{" "}
+              universe
+            </h1>
+            {/* <p className="mt-4 font-semibold text-white/60 text-xl">1643 — 1727</p> */}
+            <button className="mt-6 p-4 border transition-colors rounded-sm hover:bg-white hover:text-black flex items-center gap-2 text-lg">
+              Explore his journey <ArrowDown />
+            </button>
+          </div>
+          <div className="flex-1 items-center justify-center flex relative">
             <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+              src={"/isaac-newton.png"}
+              alt="Isaac Newton"
+              width={350}
+              height={350}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <Image
+              className="md:hidden falling-animation absolute -top-20 right-20"
+              src={"/apple.png"}
+              alt="Apple"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
-      </main>
-    </div>
+      </section>
+
+      <section
+        id=""
+        className="py-24 bg-black bg-linear-to-t from-black to-neutral-800"
+      >
+        <div className="max-w-7xl mx-auto p-8">
+          <h2 className="text-4xl font-bold text-center">
+            THE MAN BEHIND THE MIND.
+          </h2>
+          <div>
+            <p className="text-center text-white/90 mt-6">
+              <span className="text-xl font-bold">Sir Isaac Newton</span> was an
+              English polymath who was a mathematician, physicist, astronomer,
+              alchemist, theologian, author and inventor. He was a key figure in
+              the Scientific Revolution and the Enlightenment that followed. His
+              book Philosophiæ Naturalis Principia Mathematica (Mathematical
+              Principles of Natural Philosophy), first published in 1687,
+              achieved the first great unification in physics and established
+              classical mechanics. Newton also made seminal contributions to
+              optics, and shares credit with the German mathematician Gottfried
+              Wilhelm Leibniz for formulating infinitesimal calculus, although
+              he developed calculus years before Leibniz. Newton contributed to
+              and refined the scientific method, and his work is considered the
+              most influential in bringing forth modern science.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section
+        id="journey"
+        className="py-24 bg-black bg-linear-to-t from-neutral-800 to-black overflow-hidden"
+      >
+        <div className="p-8 max-w-7xl mx-auto">
+          <h2 className="text-4xl font-bold text-center">
+            BEFORE THE LAWS, THERE WAS A QUESTION.
+          </h2>
+          <p className="text-center mt-6 text-neutral-200">
+            &quot;Why does the Moon stay in the sky while an apple falls to the
+            ground?&quot;
+          </p>
+          <div className="flex items-center justify-center mt-8">
+            <Image
+              className="bouncing-animation"
+              src={"/moon.png"}
+              alt="Image of Moon"
+              width={600}
+              height={600}
+            />
+          </div>
+          {/* <div className="flex items-center justify-center"><ArrowDown className="h-10 w-10"/></div> */}
+        </div>
+      </section>
+      <section className="py-24 bg-linear-to-b from-neutral-800 to-black">
+        <div className="p-8 max-w-7xl mx-auto">
+          <h2 className="font-bold text-4xl text-center">
+            THE BOY FROM WOOLSTHORPE
+          </h2>
+          <div>
+            <div className="mt-50 flex flex-col md:flex-row items-center justify-between ">
+              <div className="flex-1">
+                <p className="text-6xl font-bold">1643</p>
+                <p className="text-lg text-white/90 mt-2">
+                  Woolsthorpe, England
+                </p>
+                <p className="max-w-md mt-6 mb-8 md:mb-0">
+                  Isaac Newton was born in a small village in Lincolnshire. His
+                  childhood was far removed from the great scientific revolution
+                  he would eventually help create.
+                </p>
+              </div>
+              <div className="flex-1">
+                <Image
+                  src={"/woolsthorpe-manor.jpg"}
+                  alt="Woolsthorpe Manor"
+                  height={500}
+                  width={500}
+                  className="rounded-sm"
+                />
+              </div>
+            </div>
+            <div className="mt-50 flex flex-col md:flex-row items-center justify-between ">
+              <div className="flex-1">
+                <p className="text-6xl font-bold">1661</p>
+                <p className="text-lg text-white/90 mt-2">CAMBRIDGE</p>
+                <p className="max-w-md mt-6 mb-8 md:mb-0">
+                  Isaac Newton was born in a small village in Lincolnshire. His
+                  childhood was far removed from the great scientific revolution
+                  he would eventually help create.
+                </p>
+              </div>
+              <div className="flex-1 relative">
+                <Image
+                  src={"/cambridge.jpg"}
+                  alt="Woolsthorpe Manor"
+                  height={500}
+                  width={500}
+                  className="rounded-sm"
+                />
+              </div>
+            </div>
+
+            <div className="mt-50 flex flex-col md:flex-row items-center justify-between ">
+              <div className="flex-1">
+                <p className="text-6xl font-bold">1665</p>
+                <p className="text-lg text-white/90 mt-2">CAMBRIDGE CLOSED.</p>
+                <p className="max-w-md mt-6 mb-8 md:mb-0">
+                  The plague forced Newton to return home.
+                </p>
+              </div>
+              <div className="flex-1 relative">
+                {/* <div className="w-[500px] h-[330px] border rounded-sm  flex items-center justify-center blur-3xl">
+                  <p className="">CLOSED</p>
+                </div> */}
+                <Image src={"/cambridge.jpg"} alt="Woolsthorpe Manor" height={500} width={500} className="rounded-sm"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24">
+        <div className="p-8 max-w-7xl mx-auto">
+          <h1 className="text-4xl text-center font-bold">
+            AND THEN, HE BEGAN <br /> TO ASK DIFFERENT QUESTIONS.
+          </h1>
+        </div>
+      </section>
+
+      <section className="py-24" id="discoveries">
+        <div className="p-8 max-w-7xl mx-auto">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-4xl font-bold text-center">THE YEAR OF WONDERS</h2>
+            <p className="text-white/90 text-center mt-6">1665–1667</p>
+            <Image
+              className=""
+              src={"/newton-writing.jfif"}
+              alt="Issac Newton's Handwriting"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24">
+        <div className="p-8 max-w-7xl mx-auto ">
+          <div className="flex items-center justify-center flex-col">
+            <h2 className="text-6xl font-bold text-center">Gravity</h2>
+            <Image
+              src={"/earth.png"}
+              alt="Image of Earth"
+              width={400}
+              height={400}
+              className="bouncing-animation mx-auto mt-8 mb-8"
+            />
+            <p className="text-lg font-medium text-white/90 text-center max-w-md mx-auto">
+              What pulls an apple toward Earth might also hold the Moon in
+              orbit.
+            </p>
+            <Link
+              href={"/"}
+              className="px-4 py-2.5 border rounded mt-8 inline-flex items-center gap-2 text-md"
+            >
+              More <ArrowRight className="w-5 h-5" />
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24">
+        <div className="p-8 max-w-7xl mx-auto">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-6xl font-bold text-center">Calculas</h2>
+            <Image
+              src={"/calculas.svg"}
+              alt="Image of Calculas"
+              width={400}
+              height={400}
+              className="bouncing-animation mx-auto mt-8 mb-8"
+            />
+            <p className="text-lg font-medium text-white/90 text-center max-w-md mx-auto">
+              Newton developed calculus to describe change and motion
+              mathematically—giving him a powerful new way to understand the
+              physical world.
+            </p>
+            <Link
+              href={"/"}
+              className="px-4 py-2.5 border rounded mt-8 inline-flex items-center gap-2 text-md"
+            >
+              More <ArrowRight className="w-5 h-5" />
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-24">
+        <div className="p-8 max-w-7xl mx-auto">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-6xl font-bold text-center">Optics</h2>
+            <Image
+              src={"/optics.webp"}
+              alt="Image of Optics"
+              width={400}
+              height={400}
+              className="bouncing-animation mx-auto mt-8 mb-8"
+            />
+            <p className="text-lg font-medium text-white/90 text-center max-w-md mx-auto">
+              Newton developed calculus to describe change and motion
+              mathematically—giving him a powerful new way to understand the
+              physical world.
+            </p>
+            <Link
+              href={"/"}
+              className="px-4 py-2.5 border rounded mt-8 inline-flex items-center gap-2 text-md"
+            >
+              More <ArrowRight className="w-5 h-5" />
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer className="bg-linear-to-b from-black to-neutral-950 border-t border-neutral-800">
+        <div className="py-20 px-5 md:px-0 max-w-7xl mx-auto">
+          <div className="flex flex-col md:flex-row  md:items-center justify-between">
+            <div>
+              <h2 className="text-4xl font-bold uppercase">Isaac Newton</h2>
+              <p className="mt-2 text-white/60">
+                The man who changed how we understand the universe.
+              </p>
+            </div>
+            <div>
+              <p className="text-xs font-bold text-white/60 tracking-[3px]">Built by</p>
+              <p className="mt-2 font-bold">Murtuza Shaikh</p>
+              <p className="mt-2 font-bold text-white/60">
+                X <a target="_blank" href="https://x.com/MurtuzaSh37756">Murtuza</a>
+              </p>
+            </div>
+          </div>
+          <div className="mt-20 border-t border-white/10 pt-6 flex flex-col md:flex-row items-start md:items-center justify-between gap-6">
+            <p className="text-sm text-white/40">© 2026 Murtuza</p>
+
+            <Link
+              href="/"
+              className="group flex items-center gap-2 border border-white/20 px-4 py-2.5 rounded-sm transition-all hover:bg-white hover:text-black"
+            >
+              Explore the universe
+              <ArrowRight className="w-5 h-5 transition-transform group-hover:translate-x-1" />
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+
+    return (
+        <header className="fixed left-0 top-0 px-4 md:px-20 py-5 flex md:flex-row items-center justify-between w-full backdrop-blur-sm z-10 bg-black/10">
+            <Link href={"/"} className="logo text-2xl font-bold">
+                Isaac Newton
+            </Link>
+            <ul className="hidden md:flex items-center gap-8 text-white/90 font-medium">
+                <li><a className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href="#">Home</a></li>
+                <li><a className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href="#journey">Journey</a></li>
+                <li><a className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href="#discoveries">Discoveries</a></li>
+                <li><a className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href="#timeline">Timeline</a></li>
+                <li><a className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href="#sources">Sources</a></li>
+                <li><Link className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href={"/more"}>More</Link></li>
+            </ul>
+            <MenuIcon className="md:hidden" onClick={() => setOpen(!open)}/>
+
+            {/* {open && (
+                <div className="bg-neutral-950">
+                <ul>
+                    <li><a className="relative hover:after:scale-100 after:origin-left after:duration-300 after:scale-0 after:content-[''] after:bg-white after:h-px after:w-full after:absolute after:-bottom-2 after:left-0" href="#">Home</a></li>
+                </ul>
+            </div>
+            )} */}
+        </header>
+    )
+}
